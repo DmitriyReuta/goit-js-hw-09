2a3c6dc49c75acd6e0616b6c7198fd57ea353a3e
Dmitriy Reuta <reutadmitriy@gmail.com>
2023-08-23 23:48:33 +0300

    aasd

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -17,30 +17,20 @@ function createPromise(position, delay) {
   }, delay);
   })
 }
-buttonSubmit.addEventListener('click', function (event) {
+form.addEventListener('submit', function (event) {
   event.preventDefault();
   let delaySome = Number(inputDelay.value);
   let stepSome = Number(inputStep.value);
   let amountSome = Number(inputAmount.value);
-  let currentPosition = 1;
 
-
-  function processPromise() {
-  if (currentPosition <= amountSome) {
-    const delay = delaySome + (currentPosition - 1) * stepSome;
-    createPromise(currentPosition, delay)
-      .then(result => {
-        console.log(`✅ Fulfilled promise ${result.position} in ${result.delay}ms`);
-      })
-      .catch(error => {
-        console.log(`❌ Rejected promise ${error.position} in ${error.delay}ms`);
-      })
-      .finally(() => {
-        delaySome +=stepSome;
-        currentPosition++;
-        processPromise();
-      });
-    }
-  } 
-  processPromise();
+  for (let i = 1; i <= amountSome; i++){
+  const delay = delaySome + (i - 1) * stepSome;
+      createPromise(i, delay)
+        .then(result => {
+          console.log(`✅ Fulfilled promise ${result.position} in ${result.delay}ms`);
+        })
+        .catch(error => {
+          console.log(`❌ Rejected promise ${error.position} in ${error.delay}ms`);
+        })
+}
 })
